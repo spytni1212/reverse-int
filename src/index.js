98dcfd79580976528ff7160f70435a84c594ee0f
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-02 20:08:08 +0300

    feat: complate task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let Arr = n.toString().split('');
+  let newArr = [];
+  for (let i = 0; i < Arr.length; i++) {
+      if (Arr[i] !== "-") {
+          newArr.push(Arr[i]);
+      }
+  }
+  newArr = newArr.reverse().join('');
+  return newArr;
 }
